b3d7e1c3d5be1a5f16cf12de772a297957bd2ddf
Muryoutaisuu <naadl.93@gmail.com>
2013-06-21 16:43:55 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,15 @@
-javascript:
-/*	-- Anleitung --
-	1.	Den Code unten in ein Lesezeichen kopieren.
-	2.	plug.dj öffnen, einloggen und einen Raum wählen
-	3.	Auf das Lesezeichen klicken und los gehts!
-	4.	mit /cmd alle Commands ausgeben
-	
-	1.	Copy the code into a bookmark
-	2.	open plug.dj, login and choose a room
-	3.	click on the bookmark and let's party hard!
-	4.	/cmd shows all commands
-*/
-
 /*	-- Code --	*/
-	/*	Sofort ausgeführter Code
-		Immediately conducted code
-	*/
+
+javascript:
+
+	/*	Immediately conducted code	*/
 	alert("Firefox: ctrl + shift + K\nweitere Befehle / more commands: /cmd");
 	
-	/*	Aktivierte EventListeners
-		Activated EventListeners
-	*/
-	/*	Funktion chat() wird bei einem neuen Chateintrag gestartet
-		chat()-function loaded on chatevent
-	*/
+	/*	Activated EventListeners	*/
+	/*	chat()-function loaded on chatevent	*/
 	API.addEventListener(API.CHAT, chat);
 	
-	/*	chat()-Funktion wird von ChatEvent gestartet
-		chat()-function loaded on chatevent
-	*/
+	/*	chat()-function loaded on chatEvent	*/
 	function chat(data)
 	{
 		var mes = data.message;
@@ -44,7 +26,7 @@ javascript:
 			default:
 				break;
 		};
-		/* Form der übertragenen Daten -- given data
+		/* given data
 		data.type
 		// "message", "emote", "moderation", "system"
 		data.from
@@ -58,17 +40,13 @@ javascript:
 		//*/
 	}
 	
-	/*	Die Funktion cmd() gibt einen link zu allen Befehlen aus
-		cmd() gives a link with a list of all commands
-	*/
+	/*	cmd() gives a link with a list of all commands	*/
 	function cmd(){
 		link = "https://github.com/Muryoutaisuu/djmury/blob/master/commands.md";
 		API.sendChat(link);
 	}
 	
-	/*	Die Funktion halloween() gibt eine Anleitung für die Halloween Charaktere aus
-		Instruction vor halloween avatars
-	*/
+	/*	Instruction for halloween avatars	*/
 	function halloween(){
 		API.sendChat("http://pastebin.com/VN1W4pAd");
 	}
