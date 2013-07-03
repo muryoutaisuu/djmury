90e87d011d0edf909a16c5704af13c5bda994998
Muryoutaisuu <naadl.93@gmail.com>
2013-07-03 11:14:50 +0200

    Update code.js

--- code.js
+++ code.js
@@ -5,6 +5,13 @@ javascript:
 	/*	Activated EventListeners	*/
 	/*	chat()-function loaded on chatevent	*/
 	API.addEventListener(API.CHAT, chat);
+	/*	nextDJ()-function loaded on DJChange-Event	*/
+	API.addEventListener(API.DJ_ADVANCE, nextDJ);
+	/*	scoreUpdate()-function loaded on Score-Update-Event	*/
+	API.addEventListener(API.ROOM_SCORE_UPDATE, scoreUpdate);
+	
+	/*	initializing global variables	*/
+	var gl_roomscore = API.getRoomScore();
 	
 	/*	chat()-function loaded on chatEvent	*/
 	function chat(data)
@@ -18,13 +25,13 @@ javascript:
 				text = cmd();
 				API.sendChat(text);
 				break;
-			case "/halloween":
-				text = halloween();
+			case "/helloween":
+				text = helloween();
 				API.sendChat(text);
 				break;
 			default:
 				break;
-		};
+		}
 		/* given data
 		data.type
 		// "message", "emote", "moderation", "system"
@@ -39,15 +46,53 @@ javascript:
 		//*/
 	}
 	
+	/*	nextDJ()-function loaded on DJChange-Event	*/
+	function nextDJ(obj){
+		string = sendScore();
+		API.sendChat(string);
+		console.log(string);
+		window.gl_roomscore = API.getRoomScore();
+	/*
+		if (obj == null) return;
+		var str = "";
+		var currentDJ = obj.dj;
+		str += currentDJ.username;
+		var total = currentDJ.djPoints + currentDJ.listenerPoints + currentDJ.curatorPoints;
+		str += " points: " + total;
+		str += ", fans: " + currentDJ.fans;
+		str += " || " + obj.media.author + " - " + obj.media.title;
+		alert(str);
+	*/
+	}
+	
 	/*	cmd() gives a link with a list of all commands	*/
 	function cmd(){
 		link = "https://github.com/Muryoutaisuu/djmury/blob/master/commands.md";
 		return link;
 	}
 	
-	/*	Instruction for halloween avatars	*/
-	function halloween(){
+	/*	Instruction for helloween avatars	*/
+	function helloween(){
 		link = "http://pastebin.com/VN1W4pAd";
 		return link;
+	}
+	
+	/*	sends score of last song	*/
+	function sendScore(){
+		var x = window.gl_roomscore;
+		var pos = x.positive;
+		var neg = x.negative;
+		var cur = x.curates;
+		var string = "/em pos: " + pos + ", neg: " + neg + ",  fav:" + cur + ".";
+		return string;
+	}
+	
+	function scoreUpdate(obj){
+		window.gl_roomscore = obj;
+	}
+	
+	/*	gets score from current song	*/
+	function getScore(){
+		return API.getRoomScore();
 	}
 ;
