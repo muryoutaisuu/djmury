8af78771bfdd2da4e937f789e02e9cb82433559d
Muryoutaisuu <naadl.93@gmail.com>
2013-07-03 13:46:15 +0200

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,8 @@
+javascript: 
+	(function(){ 
+		var jsCode = document.createElement('script');
+		jsCode.setAttribute('id', 'plugbot-js');
+		jsCode.setAttribute('src', 'https://raw.github.com/Muryoutaisuu/djmury/master/code.js');
+		document.body.appendChild(jsCode);
+	}())
+;
