e4cfa6ed09c7fc194a15dd98a75ae344d496a3b5
Muryoutaisuu <naadl.93@gmail.com>
2013-11-10 14:42:05 +0100

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:(function(){var jsCode = document.createElement('script');jsCode.setAttribute('id', 'plugbot-js');jsCode.setAttribute('src', 'https://raw.github.com/Muryoutaisuu/djmury/master/code.js');document.body.appendChild(jsCode);}());
+javascript:(function(){var jsCode=document.createElement('script');jsCode.setAttribute('id','plugbot-js');jsCode.setAttribute('src','https://raw.github.com/Muryoutaisuu/djmury/master/code.js');document.body.appendChild(jsCode);}());
